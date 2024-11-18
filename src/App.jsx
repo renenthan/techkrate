ea2c07ea8b921ac7c633ca324ee55237dfc7ae85
ChitvanJain22 <torrid22.cj@gmail.com>
2024-11-18 22:01:19 +0530

    Home Fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import About from "./pages/About";
 import Services from "./pages/Services";
 import Products from "./pages/Products";
 import Apply from "./pages/Apply";
-import HomePage from "./pages/HomePage";
+import Home from "./pages/Home";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/products" element={<Products />} />
