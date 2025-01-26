b51e9191acd833317de5ecd6fd60a5ce7285c0ba
ChitvanJain22 <torrid22.cj@gmail.com>
2025-01-26 22:17:41 +0530

    firebase api key

--- client/firebase.js
+++ client/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore"; // Import addDoc and other Firestore functions
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCx4i3qNgH4Gc8OIg37qAJeob9bZg2FFMY",
-  authDomain: "techkrate-f7efc.firebaseapp.com",
-  projectId: "techkrate-f7efc",
-  storageBucket: "techkrate-f7efc.firebasestorage.app",
-  messagingSenderId: "560803017951",
-  appId: "1:560803017951:web:4e57daf6dc4779ea10a56a"
+  apiKey: "AIzaSyCSixdJgpQOqSlC22yV_yy78axPzaXP5JQ",
+  authDomain: "techkrate-caa95.firebaseapp.com",
+  projectId: "techkrate-caa95",
+  storageBucket: "techkrate-caa95.firebasestorage.app",
+  messagingSenderId: "583632819983",
+  appId: "1:583632819983:web:f81422fd0fd366fa32907a",
 };
 
 // Initialize Firebase
